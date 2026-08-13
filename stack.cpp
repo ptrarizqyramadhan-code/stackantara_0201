2b335c217dada25d721df17d16662b885588beaf
ptrarizqyramadhan-code <ptrarizqyramadhan@gmail.com>
2026-08-13 10:22:08 +0700

    kondisi jika choise = 3

--- stack.cpp
+++ stack.cpp
@@ -116,3 +116,15 @@ int main()
                     cout << "Stack is Empty. Cannot pop." << endl;
                 }
                 break;
+
+            //18. kondisi jika choice = 3
+            case 3:
+                if (!stack.isEmpty())
+                {
+                    stack.peek();
+                }
+                else
+                {
+                    cout << "Stack is Empty. No top value." << endl;
+                }
+                break;
