80d7a4c68c8bd1fe4f40fd7d624948064936f0b0
ptrarizqyramadhan-code <ptrarizqyramadhan@gmail.com>
2026-08-13 10:05:05 +0700

    pembuatan method push

--- stack.cpp
+++ stack.cpp
@@ -28,3 +28,13 @@ public:
         //6. inisialisasi nilai awal top sebagai NULL
         top = NULL;
     }
+
+    //7. pembuatan method push
+    int push(int value) {
+        Node* newNode = new Node();
+        newNode->data = value;
+        newNode->next = top;
+        top = newNode;
+        cout << "Push Value: " << value << endl;
+        return value;
+    }
